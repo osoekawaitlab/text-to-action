4c78b39da4b4c4c504fce54b14ca9b2aec769a6e
osoken <osoken@osoekawalab.com>
2024-08-16 19:12:48 +0900

    feat: add TextToActionCore

--- src/olt2f/__init__.py
+++ src/olt2f/__init__.py
@@ -1,6 +1,11 @@
+from .core import TextToActionCore
 from .settings import TextToActionCoreSettings
 
 __version__ = "0.1.0"
 
 
-__all__ = ["TextToActionCoreSettings", "__version__"]
+__all__ = [
+    "TextToActionCoreSettings",
+    "__version__",
+    "TextToActionCore",
+]
--- tests/test_package.py
+++ tests/test_package.py
@@ -9,3 +9,7 @@ def test_olt2f_has_version() -> None:
 
 def test_olt2f_has_text_to_action_core_settings() -> None:
     assert olt2f.TextToActionCoreSettings == olt2f.settings.TextToActionCoreSettings
+
+
+def test_olt2f_has_text_to_action_core() -> None:
+    assert olt2f.TextToActionCore == olt2f.core.TextToActionCore
